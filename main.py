a447304767f9c89a210664902f6c5b0b6fa11127
Peter Zettl <mpzettl@gmail.com>
2018-05-02 02:45:19 -0500

    basic function login logout blog and signup

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from models import Blog, User
 from app import app, db
 import cgi
 
+
+
+
 @app.route('/validation', methods=['POST', 'GET'])
 def validation():
     
@@ -18,7 +21,7 @@ def validation():
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def add_post():   
-
+    
     if request.method == 'POST':
         title = request.form['post-title']
         body = request.form['post-body']
@@ -57,7 +60,7 @@ def my_blog():
 @app.route('/blog', methods=['POST','GET'])
 def single_entry():
     if request.method == 'GET':
-        
+        username = request.args.get('username')
         entry = request.args.get('id')
         blog = Blog.query.filter_by(id=entry).first()
 
@@ -74,49 +77,72 @@ def sign_up():
         username =  request.form['username']
         password = request.form['password']
         verify = request.form['verify']
-        
-        if username == "" or password =="" or verify =="":
-            fill_error = 'please fill in all areas'
-            return render_template('signup.html', username=username, fill_error=fill_error)
-        
-        elif  " " in username or " " in password:
-            username_error = 'please do not use blank spaces'
-            return  render_template('signup.html', username=username, username_error=username_error)
-        
-        elif len(username) < 3:
-            userlen_error = 'please enter a username more than 3 characters'
-            return render_template('signup.html', username=username, userlen_error=userlen_error)
-        
-        elif len(password) < 3:
-            password_error = 'please enter a password more than 3 characters'
-            return render_template('signup.html', username=username,  password_error=password_error)
+        user_exists = User.query.filter_by(username=username).first()
+        if not user_exists:
+            if username == "" or password =="" or verify =="":
+                fill_error = 'please fill in all areas'
+                return render_template('signup.html', username=username, fill_error=fill_error)
+            
+            elif  " " in username or " " in password:
+                username_error = 'please do not use blank spaces'
+                return  render_template('signup.html', username=username, username_error=username_error)
+            
+            elif len(username) < 3:
+                userlen_error = 'please enter a username more than 3 characters'
+                return render_template('signup.html', username=username, userlen_error=userlen_error)
+            
+            elif len(password) < 3:
+                password_error = 'please enter a password more than 3 characters'
+                return render_template('signup.html', username=username,  password_error=password_error)
 
-        elif password == verify:
-            new_user = User(username, password)
-                
-            db.session.add(new_user)
-            db.session.commit()
-            blog = Blog.query.all()
+            elif password == verify:
+                new_user = User(username, password)
+                    
+                db.session.add(new_user)
+                db.session.commit()
+                blog = Blog.query.all()
+            
+                return render_template('base.html', username=username)
         
-    
-            return render_template('base.html', username=username)
-    
+            else:
+                verify_error = 'please make sure your verification matches your password!'
+                return render_template('signup.html', username=username, verify_error=verify_error)  
         else:
-            verify_error = 'please make sure your verification matches your password!'
-            return render_template('signup.html', username=username, verify_error=verify_error)  
+            error = "Username exists"
+            return render_template('signup.html', error=error)
     else:
         return render_template('signup.html')
+@app.route('/login', methods=['POST', 'GET'])
+def log_in():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        
+        if user and user.password == password:
 
+            #return redirect('/newpost')
+            return render_template('newpost.html', username=username)
+        else:
+            error = "Password incorrect or user does not exist"
 
+            return render_template('login.html', username=username, error=error)
+    else:
+        return render_template('login.html')
 
-
-
+@app.route('/logout', methods=['POST', 'GET'])
+def log_out():
+    #del session['username']
+    return redirect('/')
+#@app.route('/home', methods=['POST', 'GET'])
+    
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method =='GET':
         
-        return render_template('signup.html')
-    else:   
+        return render_template('login.html')
+    else:
+        #username = User.query.filter_by(id=id).first()  
         return render_template('base.html', username=username)
 
 if __name__ == '__main__':
